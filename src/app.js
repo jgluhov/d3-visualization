445457507a3fbe89d1489316cd5b78d189e157ab
Evgeny <jgluhov@gmail.com>
2017-10-29 21:47:31 +0300

    add bars

--- src/app.js
+++ src/app.js
@@ -1,3 +1,35 @@
-import {version} from 'd3';
+import {select} from 'd3-selection';
 
-document.body.innerHTML = `<h1>Hello from ${version}</h1>`;
+const scores = [
+  { name: 'Alice', score: 95 },
+  { name: 'Mamba', score: 60 },
+  { name: 'Triple', score: 31 },
+  { name: 'Quarterback', score: 11 },
+  { name: 'Houston', score: 77 },
+  { name: 'Keats', score: 58 }
+];
+
+const alphabetize = (array) => {
+  return array.sort((a, b) => {
+    if (a.name < b.name) {
+      return -1;
+    }
+
+    if (a.name > b.name) {
+      return 1;
+    }
+
+    return 0;
+  })
+};
+
+
+select('.chart')
+  .selectAll('div')
+  .data(scores)
+  .enter()
+  .append('div')
+  .style('height', d => d.score + 'px')
+  .style('width', '50px')
+  .classed('bar', true);
+
