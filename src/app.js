fa289a8674cdac25f88627f08b786be15fc90369
Evgeny <jgluhov@gmail.com>
2017-10-29 17:56:48 +0300

    trying selection

--- src/app.js
+++ src/app.js
@@ -1,3 +1,14 @@
-import {version} from 'd3';
+import {select, selectAll} from 'd3-selection';
 
-document.body.innerHTML = `<h1>Hello from ${version}</h1>`;
+const title = select('.title'),
+  titleLinks = title.selectAll('a'),
+  actionLink = select('.action'),
+  secondLink = selectAll('.title > a:nth-child(2)');
+
+console.log(titleLinks.nodes());
+console.log(actionLink.nodes());
+console.log(secondLink.nodes());
+
+const allLinks = selectAll(document.links);
+console.log(allLinks.nodes());
+console.log(allLinks.size());
