79627545d0c2864787ddd75714a1d7988a66d968
Evgeny <jgluhov@gmail.com>
2017-10-29 18:42:50 +0300

    trying selection, creating, styling

--- src/app.js
+++ src/app.js
@@ -5,10 +5,34 @@ const title = select('.title'),
   actionLink = select('.action'),
   secondLink = selectAll('.title > a:nth-child(2)');
 
-console.log(titleLinks.nodes());
-console.log(actionLink.nodes());
-console.log(secondLink.nodes());
+// console.log(titleLinks.nodes());
+// console.log(actionLink.nodes());
+// console.log(secondLink.nodes());
 
 const allLinks = selectAll(document.links);
-console.log(allLinks.nodes());
-console.log(allLinks.size());
+// console.log(allLinks.nodes());
+// console.log(allLinks.size());
+
+// secondLink.attr('href', 'http://google.com');
+
+selectAll('.title > a:nth-child(2)')
+  .attr('href', 'http://google.com');
+
+selectAll('.title > a:nth-child(2)')
+  .classed('red', true);
+
+selectAll('.title > a:nth-child(2)')
+  .text('Inventory');
+
+selectAll('.title > a:nth-child(2)')
+  .html('Inventory <b>SALE</b>');
+
+select('.title')
+  .append('div')
+    .classed('red', true)
+    .html('Inventory <b>SALE</b>')
+  .append('button')
+    .style('display', 'block')
+    .text('submit');
+
+select('.action').remove();
