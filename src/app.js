2a6544eabfbf3e1cf18f4e9bfbd64c985daccec8
Evgeny <jgluhov@gmail.com>
2017-10-29 15:14:07 +0300

    trying to scale

--- src/app.js
+++ src/app.js
@@ -1,3 +1,29 @@
-import {version} from 'd3';
+import {
+  scaleLinear,
+  scaleTime
+} from 'd3-scale';
 
-document.body.innerHTML = `<h1>Hello from ${version}</h1>`;
+const linearScale = scaleLinear()
+  .domain([0, 100])
+  .range([- Math.PI / 6, 7 * Math.PI / 6])
+  .clamp(true);
+
+// console.log(linearScale(0));
+// console.log(linearScale(50));
+// console.log(linearScale(100));
+//
+// console.log(linearScale.invert(0));
+// console.log(linearScale.invert(50));
+// console.log(linearScale.invert(100));
+
+
+const timeScale = scaleTime()
+  .domain([new Date(2017, 0, 1), new Date()])
+  .range([0, 100])
+  .clamp(true);
+
+// console.log(timeScale(new Date(2017, 0, 1)));
+// console.log(timeScale(new Date(2017, 5, 14)));
+// console.log(timeScale(new Date()));
+//
+// console.log(timeScale.invert(50));
