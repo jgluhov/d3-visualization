700a2c32e36997151e5d512cf05c7348c6c8336f
Evgeny <jgluhov@gmail.com>
2017-10-29 17:22:25 +0300

    trying to work with data.

--- src/app.js
+++ src/app.js
@@ -1,3 +1,28 @@
-import {version} from 'd3';
+import {scaleLinear} from 'd3-scale'
+import {json} from 'd3-request';
+import {min, max, extent} from 'd3-array';
+import {set} from 'd3-collection'
 
-document.body.innerHTML = `<h1>Hello from ${version}</h1>`;
+json('data/data.json', (data) => {
+  const minAge = min(data, (d) => d.age),
+    maxAge = max(data, (d) => d.age),
+    extentAge = extent(data, (d) => d.age);
+
+  console.log('minAge', minAge);
+  console.log('maxAge', maxAge);
+  console.log('extentAge', extentAge);
+
+  const scale = scaleLinear()
+    .domain(extentAge)
+    .range([0, 600]);
+
+  console.log(scale(22));
+
+  const ages = set(data, (d) => d.age);
+
+  console.log(ages.values());
+});
+
+// csv('data/data.csv', (data) => {
+//   console.log(data);
+// });
