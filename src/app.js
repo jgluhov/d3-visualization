e3d8876b5896512e9d75297544cca3c67b3f63ac
Evgeny <jgluhov@gmail.com>
2017-11-03 16:36:42 +0300

    add bars with svg

--- src/app.js
+++ src/app.js
@@ -1,3 +1,29 @@
-import {version} from 'd3';
+import {select} from 'd3-selection';
 
-document.body.innerHTML = `<h1>Hello from ${version}</h1>`;
+const scores = [
+  { name: '1', score: 1 },
+  { name: '2', score: 2 },
+  { name: '3', score: 3 },
+  { name: '4', score: 4 },
+  { name: '5', score: 5 },
+  { name: '6', score: 6 }
+];
+
+const bar = select('.chart')
+    .append('svg')
+        .attr('width', 200)
+        .attr('height', 300)
+    .selectAll('g')
+    .data(scores)
+    .enter()
+        .append('g')
+        .attr('transform', (d, i) => `translate(0, ${i * 30})`);
+
+bar.append('rect')
+    .style('width', d => d.score * 30)
+    .classed('bar', true);
+
+bar.append('text')
+    .attr('y', 20)
+    .text(d => d.name)
+    .classed('text', true);
