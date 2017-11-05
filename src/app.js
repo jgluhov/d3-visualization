b5fc255f1afd39dd05b42d0e2d0121b2f9077640
evglukhov <evglukhov@luxoft.com>
2017-11-05 13:56:56 +0300

    margins

--- src/app.js
+++ src/app.js
@@ -1,3 +1,31 @@
-import {version} from 'd3';
+import { select } from 'd3-selection';
 
-document.body.innerHTML = `<h1>Hello from ${version}</h1>`;
+const margin = {
+    top: 10,
+    right: 15,
+    bottom: 10,
+    left: 15
+};
+
+const width = 425 - margin.left - margin.right;
+const height = 625 - margin.top - margin.bottom;
+
+const svg = select('.chart')
+    .append('svg')
+        .attr('width', width + margin.left + margin.right)
+        .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+        .attr('transform', `translate(${margin.left},${margin.top})`);
+
+svg.append('rect')
+    .attr('width', width / 2)
+    .attr('height', height)
+    .style('fill', 'lightblue')
+    .style('stroke', 'green');
+
+svg.append('rect')
+    .attr('x', width / 2)
+    .attr('width', width / 2)
+    .attr('height', height)
+    .style('fill', 'lightblue')
+    .style('stroke', 'green');
