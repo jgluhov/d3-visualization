058570be0028235dabae7cbb6637d7689cac8674
Evgeny <jgluhov@gmail.com>
2017-11-08 14:46:42 +0300

    clean

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
 import { select } from 'd3-selection';
-import { scaleLinear, scaleTime, } from 'd3-scale';
+import { scaleLinear } from 'd3-scale';
 import { axisLeft, axisBottom } from 'd3-axis';
-import { timeMinute } from 'd3-time';
 
 const margin = {
     top: 5,
-    right: 0,
+    right: 15,
     bottom: 40,
     left: 30
 };
@@ -21,33 +20,21 @@ const svg = select('.chart')
     .append('g')
         .attr('transform', `translate(${margin.left},${margin.top})`);
 
-svg.append('rect')
-    .attr('width', width)
-    .attr('height', height)
-    .style('fill', 'lightblue')
-    .style('stroke', 'green');
-
 const yScale = scaleLinear()
     .domain([0, 100])
     .range([height, 0]);
 
-const yAxis = axisLeft(yScale)
-    .ticks(10);
+const yAxis = axisLeft(yScale);
 
 svg.append('g')
     .call(yAxis);
 
-const xScale = scaleTime()
-    .domain([new Date(2017, 0, 1, 6), new Date(2017, 0, 1, 9)])
+const xScale = scaleLinear()
+    .domain([0, 100])
     .range([0, width]);
 
 
-const xAxis = axisBottom(xScale)
-    .ticks(timeMinute.every(60))
-    .tickSize(10)
-    .tickSizeInner(10)
-    .tickSizeOuter(20)
-    .tickPadding(10);
+const xAxis = axisBottom(xScale);
 
 svg.append('g')
     .attr('transform', `translate(0, ${height})`)
