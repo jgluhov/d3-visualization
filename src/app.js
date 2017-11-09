78689da79015a5f9d1910edfff518809c93c2e84
Evgeny <jgluhov@gmail.com>
2017-11-09 14:55:33 +0300

    update

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ import { select } from 'd3-selection';
 import { scaleLinear, scaleBand } from 'd3-scale';
 import { axisLeft, axisBottom } from 'd3-axis';
 import { timeMinute } from 'd3-time';
+import { transition } from 'd3-transition';
+
+import { json } from 'd3-request';
 
 const margin = {
     top: 5,
@@ -21,56 +24,79 @@ const svg = select('.chart')
     .append('g')
         .attr('transform', `translate(${margin.left},${margin.top})`);
 
-const data = [
-    {score: 63, subject: 'Mathematics'},
-    {score: 82, subject: 'Geography'},
-    {score: 74, subject: 'Spelling'},
-    {score: 57, subject: 'Reading'},
-    {score: 52, subject: 'Science'}
-];
 
-const xScale = scaleBand()
-    .paddingInner(0.2)
-    .paddingOuter(0.1)
-    .domain(data.map(d => d.subject))
-    .range([0, width]);
+json('data/update-data.json', (err, data) => {
+    const xScale = scaleBand()
+        .paddingInner(0.2)
+        .paddingOuter(0.1)
+        .domain(data.map(d => d.name))
+        .range([0, width]);
 
-const yScale = scaleLinear()
-    .domain([0, 100])
-    .range([height, 0]);
+    const yScale = scaleLinear()
+        .domain([0, 100])
+        .range([height, 0]);
 
-svg.selectAll('rect')
-    .data(data)
-    .enter()
-    .append('rect')
-    .attr('x',  d => xScale(d.subject))
-    .attr('y', d => yScale(d.score))
-    .attr('width', d => xScale.bandwidth())
-    .attr('height', d => height - yScale(d.score))
-    .style('fill', 'steelblue');
+    const yAxis = axisLeft(yScale)
+        .ticks(10);
 
-const yAxis = axisLeft(yScale)
-    .ticks(10);
+    const xAxis = axisBottom(xScale)
+        .ticks(timeMinute.every(60))
+        .tickSize(10)
+        .tickSizeInner(10)
+        .tickSizeOuter(20)
+        .tickPadding(10);
 
-const xAxis = axisBottom(xScale)
-    .ticks(timeMinute.every(60))
-    .tickSize(10)
-    .tickSizeInner(10)
-    .tickSizeOuter(20)
-    .tickPadding(10);
-
-svg.append('g')
-    .attr('transform', `translate(0, ${height})`)
-    .call(xAxis)
-    .selectAll('text')
+    svg.append('g')
+        .attr('transform', `translate(0, ${height})`)
+        .call(xAxis)
+        .selectAll('text')
         .style('text-anchor', 'end')
         .style('transform', 'rotate(-45deg)');
 
-svg.append('g')
-    .call(yAxis)
-    .selectAll('text')
+    svg.append('g')
+        .call(yAxis)
+        .selectAll('text')
         .style('text-anchor', 'end')
         .style('transform', 'rotate(-45deg)');
+
+    function render (subject = 'math') {
+        const t = transition()
+            .duration(1000);
+
+        const update = svg.selectAll('rect')
+            .data(data.filter(d => d[subject]), d => d.name);
+
+        update
+            .exit()
+            .attr('y', height)
+            .attr('height', 0)
+            .transition(t)
+            .remove();
+
+        update
+            .transition(t)
+            .delay(1000)
+            .attr('y', d => yScale(d[subject]))
+            .attr('height', d => height - yScale(d[subject]));
+
+        update
+            .enter()
+            .append('rect')
+            .attr('y', height)
+            .attr('height', 0)
+            .attr('x',  d => xScale(d.name))
+            .attr('width', d => xScale.bandwidth())
+            .transition(t)
+            .delay(300)
+            .attr('y', d => yScale(d[subject]))
+            .attr('height', d => height - yScale(d[subject]))
+            .attr('class', 'rect');
+    }
+
+    render();
+
+    window.render = render;
+});
 
 function responsify(svg) {
     const container = select(svg.node().parentNode),
