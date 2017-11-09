a295f15f613472444cb6f32c14b6d6e29863fa50
Evgeny <jgluhov@gmail.com>
2017-11-09 13:06:40 +0300

    add animation and transition

--- src/app.js
+++ src/app.js
@@ -1,3 +1,18 @@
-import {version} from 'd3';
+import { select } from 'd3-selection';
+import 'd3-transition';
+import { easeBounceOut, easeQuadOut } from 'd3-ease';
 
-document.body.innerHTML = `<h1>Hello from ${version}</h1>`;
+select('#block')
+    .transition()
+        .duration(1000)
+        .delay(150)
+        .ease(easeBounceOut)
+        .style('width', '400px')
+    .transition()
+        .duration(1000)
+        .ease(easeBounceOut)
+        .style('height', '400px')
+    .transition()
+        .duration(1000)
+        .ease(easeQuadOut)
+        .style('background-color', 'yellow');
