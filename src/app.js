7c92d3a009a7a5858272e835308edadc1c2bf9a9
evglukhov <evglukhov@luxoft.com>
2017-11-09 13:34:48 +0300

    add reused animation

--- src/app.js
+++ src/app.js
@@ -1,18 +1,36 @@
-import { select } from 'd3-selection';
-import 'd3-transition';
-import { easeBounceOut, easeQuadOut } from 'd3-ease';
+import { select, selectAll } from 'd3-selection';
+import { transition } from 'd3-transition';
+import { easeElasticInOut } from 'd3-ease';
 
-select('#block')
-    .transition()
+function go() {
+    const t = transition()
+        .delay(1000)
         .duration(1000)
-        .delay(150)
-        .ease(easeBounceOut)
-        .style('width', '400px')
-    .transition()
-        .duration(1000)
-        .ease(easeBounceOut)
-        .style('height', '400px')
-    .transition()
-        .duration(1000)
-        .ease(easeQuadOut)
-        .style('background-color', 'yellow');
+        .ease(easeElasticInOut);
+
+    selectAll('.block')
+        .transition(t)
+        .style('width', '400px');
+
+    select('.block--a')
+        .transition(t)
+        .style('background-color', 'orange');
+
+    select('.block--b')
+        .transition(t)
+        .style('opacity', 0.5);
+}
+
+function configure(t, delay, duration) {
+    return t.delay(delay).duration(duration);
+}
+
+function goNow() {
+    selectAll('.block')
+        .transition()
+        .call(configure, 100, 1000)
+        .style('height', '400px');
+}
+
+window.go = go;
+window.goNow = goNow;
